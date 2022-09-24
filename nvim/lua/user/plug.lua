02529a7c57c2b15654ce8e7b3c1e1e0bd782205d
Suman714 <63091437+Suman714@users.noreply.github.com>
2022-09-24 22:34:36 +0530

    Delete nvim directory

--- nvim/lua/user/plug.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd [[
-call plug#begin()
-  Plug 'nvim-telescope/telescope.nvim', { 'tag': '0.1.0' }
-  Plug '~/.fzf'
-  Plug 'junegunn/fzf.vim'
-  Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-call plug#end()
-]]
